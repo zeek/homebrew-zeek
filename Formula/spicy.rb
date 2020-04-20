4013f3240d7f198fd60c95bb6baf4ed5fb55753e
Benjamin Bannier <bbannier@gmail.com>
2020-04-20 11:27:51 +0200

    Add HEAD-only formula for Spicy

---
+++ Formula/spicy.rb
@@ -0,0 +1,26 @@
+class Spicy < Formula
+  desc "C++ parser generator for dissecting protocols & files"
+  homepage "https://github.com/zeek/spicy"
+  head "https://github.com/zeek/spicy.git"
+
+  depends_on "bison" => :build
+  depends_on "cmake" => :build
+  depends_on "flex" => :build
+  depends_on "llvm"
+  depends_on "zeek"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args,
+                      "-DFLEX_ROOT=#{Formula["flex"].opt_prefix}",
+                      "-DBISON_ROOT=#{Formula["bison"].opt_prefix}",
+                      "-DLLVM_ROOT=#{Formula["llvm"].opt_prefix}",
+                      "-DHILTI_HAVE_JIT=ON"
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match "clang", shell_output("#{bin}/spicy-config --jit-compiler")
+  end
+end
